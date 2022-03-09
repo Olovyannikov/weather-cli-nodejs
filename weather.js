aa6fa4260bc2025353b68943fa135e344a96808f
Илья Оловянников <ilaolovannikov@MacBook-Pro-Ila>
2022-03-09 17:45:36 +0300

    Add Api key

--- weather.js
+++ weather.js
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
 import {getArgs} from './helpers/args.js';
+import {printError, printHelp, printSuccess} from "./services/log.service.js";
+import {saveKeyValue, TOKEN_DICTIONARY} from "./services/storage.service.js";
+import {getWeather} from "./services/api.service.js";
+
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Token is saved');
+    } catch (e) {
+        printError(e.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
-    console.log(args);
+
+    if (args.h) {
+        printHelp();
+    }
+
+    if (args.s) {
+    }
+
+    if (args.t) {
+        return saveToken(args.t);
+    }
+
+    getWeather('moscow');
 };
 
 initCLI();
